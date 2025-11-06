ff40eca68fc3342126dff35d2d93abebde671458
sir-ff <vadislavkusmacev@gmail.com>
2025-11-06 18:58:33 +0300

    Add files via upload

---
+++ Modul3Part5/Task.py
@@ -0,0 +1,125 @@
+def figura1():
+    #a
+    print("*****")
+    print(" ****")
+    print("  ***")
+    print("   **")
+    print("    *")
+
+
+
+def figura2():
+    #б
+    print("*")
+    print("**")
+    print("***")
+    print("****")
+    print("*****")
+
+
+def figura3():
+    #в
+    print("*****")
+    print(" *** ")
+    print("  *  ")
+
+
+def figura4():
+    #г
+    print("  *  ")
+    print(" *** ")
+    print("*****")
+def figura5():
+    #д
+    print("*****")
+    print(" *** ")
+    print("  *  ")
+    print(" *** ")
+    print("*****")
+def figura6():
+    #е
+    print("*   *")
+    print("** **")
+    print("*****")
+    print("** **")
+    print("*   *")
+
+
+
+def figura7():
+    #ж
+    print("*  ")
+    print("** ")
+    print("***")
+    print("** ")
+    print("*  ")
+
+
+def figura8():
+    #з
+    print("  *")
+    print(" **")
+    print("***")
+    print(" **")
+    print("  *")
+
+
+def figura9():
+    #и
+    print("*****")
+    print("**** ")
+    print("***  ")
+    print("**   ")
+    print("*    ")
+
+
+def figura10():
+    #к
+    print("    *")
+    print("   **")
+    print("  ***")
+    print(" ****")
+    print("*****")
+def figura11():
+    #1
+    print("*****  *  *  *****  *****  *****  *****")
+    print("*      * *   *      *      *   *  *    ")
+    print("*      *     *****  *****  *   *  *****")
+    print("*      * *   *      *   *  *   *  *   *")
+    print("*****  *  *  *****  *****  *****  *****")
+
+
+print("Привет! Эта программа рисует фигуры из звездочек, чтобы завершить напигите стоп, НЕПИСАТЬ 1 ПРОСТО ДОВЕРЬСЯ МНЕ!!!")
+print("Выбери фигуру от а до к")
+
+while True:
+    choice= input("Твой выбор а-к: ")
+
+    if choice == "а":
+        figura1()
+    elif choice == "б":
+        figura2()
+    elif choice == "в":
+        figura3()
+    elif choice == "г":
+        figura4()
+    elif choice == "д":
+        figura5()
+    elif choice == "е":
+        figura6()
+    elif choice == "ж":
+        figura7()
+    elif choice == "з":
+        figura8()
+    elif choice == "и":
+        figura9()
+    elif choice == "к":
+        figura10()
+    elif choice == "1":
+        figura11()
+    elif choice == "0":
+        break
+    else:
+        print("ты что король ЛОУТАБА???????????? пиши от а до к дундунк ёмае!!! ну или 0 чтобы заверишить.")
+
+print("ну все насмотрелся,а 1 Ввел??????,НЕТ????,а ну Вводи давай)!!!!!!,а если ввел то красава)))")
